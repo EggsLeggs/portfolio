ad92b89af6204bcddd54aec0912ec3f7b5634187
Amory <haybrad@live.co.uk>
2022-11-04 22:54:32 +0000

    this is very very very very broken and bad

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <vue-scroll-snap :fullscreen="true" :horizontal="true">
-      <div class="item" id="one"><p>test</p></div>
-      <div class="item" id="two"><p>test2</p></div>
-      <div class="item" id="three"><p>test3</p></div>
-      <div class="item" id="four"><p>test4</p></div>
-  </vue-scroll-snap>
-</template>
-
-<script>
-
+    <vue-scroll-snap :fullscreen="true" :horizontal="true">
+        <div class="item" id="one"><p>test</p></div>
+        <div class="item" id="two"><p>test2</p></div>
+        <div class="item" id="three"><p>test3</p></div>
+        <div class="item" id="four"><p>test4</p></div>
+    </vue-scroll-snap>
+  </template>
+  
+  <script>
+  import VueScrollSnap from '../components/VueScrollSnap.vue';
   export default {
-      components: {}
+    components: {
+      'vue-scroll-snap': VueScrollSnap,
+    },
   };
-</script>
+  </script>
