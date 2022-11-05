82f57a1d4aaa9ffc468ce70a4814c381c0aafec6
Amory <haybrad@live.co.uk>
2022-11-05 11:01:15 +0000

    pls work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static',
+    router: {
+        base: '/portfolio/' // base: '/<repository>/'
+    },
     modules: ['@nuxtjs/tailwindcss'],
     components: true,
 })
