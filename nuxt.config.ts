d2aaab602a15300a25d818c838d81d1fb816ebcf
Hayden B <haybrad@live.co.uk>
2022-11-06 00:06:50 +0000

    fixed config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     router: {
-        base: '/portfolio/' // base: '/<repository>/'
-    },
+        base: '/'
+      },
     modules: ['@nuxtjs/tailwindcss'],
     components: true,
 })
