f3a1688f5b53001a1c4e13eeb7aeb4c7f82fea98
Hayden B <haybrad@live.co.uk>
2022-11-06 00:57:19 +0000

    fixing 404 errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 import { defineNuxtConfig } from 'nuxt/config'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
     router: {
-        base: '/'
+        base: '/portfolio/'
       },
     modules: ['@nuxtjs/tailwindcss'],
     components: true,
